1cf41828d5a458568a12868cd2ecfcc7813ce76b
mansari <mo@ansari.com>
2020-01-21 11:18:16 -0700

    updated README.md removed IT tests

--- spark_commons/src/main/scala/code/camp/spark/commons/StringUdfs.scala
+++ spark_commons/src/main/scala/code/camp/spark/commons/StringUdfs.scala
@@ -10,18 +10,6 @@ import org.apache.spark.sql.functions.udf
 
 object StringUdfs {
 
-  def replaceReservedCharUdf(): UserDefinedFunction = {
-    val urlCleaner = (s: String) => {
-      if (s == null){
-        null
-      }  else {
-        s.replace("\t", " ").replace("\n", "").replace("\r", "")
-      }
-    }
-    val udfFunc = udf(urlCleaner)
-    udfFunc
-  }
-
   def retainAlphaNumUdf(): UserDefinedFunction = {
     val urlCleaner = (s: String) => {
       if (s == null){
